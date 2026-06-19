320c83f00ec78e9f439c2b1db7514f8ca4630cbc
mayuxiang <24772045+mayuxiang@users.noreply.github.com>
2026-06-19 12:24:37 +0800

    Fix redirect: inline JS in <head> instead of external file for immediate redirect

--- static/js/redirect.js
+++
@@ -1,8 +0,0 @@
-// Redirect mayuxiang.github.io → mayuxiang.com
-(function () {
-  var host = window.location.hostname;
-  if (host === 'mayuxiang.github.io') {
-    var target = 'https://mayuxiang.com' + window.location.pathname + window.location.search + window.location.hash;
-    window.location.replace(target);
-  }
-})();
